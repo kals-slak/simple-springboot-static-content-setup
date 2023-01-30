32a5840a8391e7947b40ad96a211e8928b379730
Kalyana Sundaram <sundaram.kalyan369@gmail.com>
2023-01-30 16:35:07 +0530

    added files

---
+++ src/main/java/com/example/demo/StaticHtmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticHtmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticHtmlApplication.class, args);
+	}
+
+}
